de3b65a0bc73d6b01a957a861d901e5c3e5f7739
Matias Domingues <mannias@gmail.com>
2011-08-25 12:05:48 -0300

    Change-Id: Ic13716feb327cbc09fca28b6979164c0c0bed371

--- Practica 3/src/Ejercicio4/Parse.java
+++ Practica 3/src/Ejercicio4/Parse.java
@@ -8,14 +8,15 @@ import java.util.regex.Pattern;
 
 public class Parse {
 
-	private Pattern pat1 = Pattern.compile(".*\\begin{(.+)}.*");
-	private Pattern pat2 = Pattern.compile(".*\\end{(.+)}.*");
+	
 
 	public boolean parser(String fileName) throws IOException {
 		String line;
 		BufferedReader in = new BufferedReader(new FileReader(fileName));
 		Stack<String> data = new Stack<String>();
 		while ((line = in.readLine()) != null) {
+			Pattern pat1 = Pattern.compile(".*\\begin{(.+)}.*");
+			Pattern pat2 = Pattern.compile(".*\\end{(.+)}.*");
 			Matcher matcher = pat1.matcher(line);
 			while (matcher.find()) {
 				data.push(matcher.group(1));

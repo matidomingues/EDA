c53b08ea74a1da85b27327b7914de7910ac1545c
Matias Domingues <mannias@gmail.com>
2011-08-25 19:27:48 -0300

    la

--- Practica 3/src/Ejercicio4/Parse.java
+++ Practica 3/src/Ejercicio4/Parse.java
@@ -12,17 +12,20 @@ public class Parse {
 
 	public boolean parser(String fileName) throws IOException {
 		String line;
-		BufferedReader in = new BufferedReader(new FileReader("C:/Users/mati/git/EDA/Practica 3/src/Ejercicio4/"+fileName));
+		BufferedReader in = new BufferedReader(new FileReader("/home/matdomin/EDA/Practica 3/src/Ejercicio4/"+fileName));
 		Stack<String> data = new Stack<String>();
 		while ((line = in.readLine()) != null) {
-			Pattern pat1 = Pattern.compile(".*\\begin{(.+)}.*");
-			Pattern pat2 = Pattern.compile(".*\\end{(.+)}.*");
+			System.out.println(line);
+			Pattern pat1 = Pattern.compile("\\begin([^}]*)");
+			Pattern pat2 = Pattern.compile("\\end(.+)");
 			Matcher matcher = pat1.matcher(line);
 			while (matcher.find()) {
+				System.out.println(matcher.group(1));
 				data.push(matcher.group(1));
 			}
 			matcher = pat2.matcher(line);
 			while (matcher.find()) {
+				System.out.println(matcher.group(1));
 				if (data.pop() != matcher.group(1)) {
 					return false;
 				}
